905ed8e612e16f096bea2ae396bec871c254cefb
barrettotte <barrettotte@gmail.com>
2022-08-08 23:32:36 -0400

    completed hash with test cases

--- md5.pas
+++ md5.pas
@@ -1,22 +1,23 @@
 program MD5;
 
+uses sysutils; (* IntToHex *)
+
 type
-    DigestBuffer = array[0..4] of longword; // A,B,C,D
     TestCase = record
         Msg, Hash: string;
     end;
 
 const
+    (* initial values of registers *)
     InitA: longword = $67452301;
-    InitB: longword = $efcdaB89;
+    InitB: longword = $efcdab89;
     InitC: longword = $98badcfe;
     InitD: longword = $10325476;
 
-    Shifts: array[0..15] of integer = (
-        7, 12, 17, 22, 5, 9, 14, 20, 
-        4, 11, 16, 23, 6, 10, 15, 21
-    );
+    (* per-round shift amounts *)
+    Shifts: array of longword = (7, 12, 17, 22, 5, 9, 14, 20, 4, 11, 16, 23, 6, 10, 15, 21);
 
+    (* Binary integer parts of sines of integers (in radians) *)
     T: array[0..63] of longword = (
         $d76aa478, $e8c7b756, $242070db, $c1bdceee,
         $f57c0faf, $4787c62a, $a8304613, $fd469501,
@@ -36,6 +37,7 @@ const
         $f7537e82, $bd3af235, $2ad7d2bb, $eb86d391
     );
 
+    (* test cases from RFC-1321 *)
     TestCases: array[0..6] of TestCase = (
         (Msg: ''; Hash: '0xd41d8cd98f00b204e9800998ecf8427e'),
         (Msg: 'a'; Hash: '0x0cc175b9c0f1b6a831c399e269772661'),
@@ -49,80 +51,169 @@ const
 (* Initialize T. T[i] = 2^32 * abs(sin(i+1)) *)
 procedure InitT;
 var 
-    i: Integer;
+    i: integer;
 begin
     for i := 0 to 63 do
     begin
-        T[i] := trunc((1 << 32) * abs(sin(i + 1)));
-        (* writeln (T[i]); *)
+        T[i] := Trunc((uint64(1) shl 32) * Abs(Sin(i + 1.0)));
     end;
-    (* NOTE: Decided to go with compile time array, but this still works *)
+    (* NOTE: Decided to go with compile time array, but this still works if T is redeclared as variable *)
 end;
 
-(* Auxiliary function 1. F(X,Y,Z) = XY OR NOT(X) Z *)
-function AuxF(x, y, z: longword): longword;
-begin
-    AuxF := (x and y) or ((not x) and z);
-end;
-
-(* Auxiliary function 2. G(X,Y,Z) = XZ OR Y NOT(Z) *)
-function AuxG(x, y, z: longword): longword;
-begin
-    AuxG := (x and z) or (y and (not z));
-end;
-
-(* Auxiliary function 3. H(X,Y,Z) = X XOR Y XOR Z *)
-function AuxH(x, y, z: longword): longword;
-begin
-    AuxH := x xor y xor z;
-end;
-
-(* Auxiliary function 4. I(X,Y,Z) = Y XOR (X OR NOT(Z)) *)
-function AuxI(x, y, z: longword): longword;
-begin
-    AuxI := y xor (x or (not z));
-end;
-
-(* Rotate x left by n-bits (modulo 32) *)
+(* Rotate x left by n-bits *)
 function RotateLeft32(x, n: longword): longword;
 begin
-    RotateLeft32 := (x << n) or (x >> (32 - n));
+    RotateLeft32 := (x shl n) or (x shr (32 - n));
 end;
 
 (* Compute MD5 hash of given string *)
 function ComputeMD5(msg: string): string;
 var
-    i, j: integer;
-    msgLenBytes, msgLenBits, paddedLen, blockCount, totalLen: longword;
-    a, b, c, d, tmpA, tmpB, tmpC, tmpD: longword;
+    i, j, buffIdx: integer;
+    a, b, c, d, a0, b0, c0, d0: longword;
+    tmp, round, f, msgBytes, msgBits, blocks, padLen: longword;
     padded: packed array of byte;
-    block: packed array[0..15] of integer;
+    buffer: packed array[0..15] of longword;
+    hash: packed array[0..15] of byte;
+    hashStr: string;
 begin
     (* calculate padding *)
-    msgLenBytes := Length(msg);
-    msgLenBits := msgLenBytes << 3;
-    blockCount := ((msgLenBytes + 8) >> 6) + 1;
-    totalLen := blockCount << 6;
-    paddedLen := totalLen - msgLenBytes;
+    msgBytes := Length(msg);
+    msgBits := msgBytes * 8;
+    blocks := ((msgBytes + 8) shr 6) + 1;
+    padLen := (blocks shl 6) - msgBytes;
+    SetLength(padded, padLen);
 
     (* pad message *)
-    setLength(padded, paddedLen);
-    padded[0] := $80; // append bit (0b1000000)
-    for i := 0 to 8 do
+    padded[0] := $80;
+    for i := 0 to 7 do
     begin
-        padded[paddedLen - 8 + i] := msgLenBits;
-        msgLenBits := msgLenBits >> 8;
+        padded[padLen - 8 + i] := msgBits;
+        msgBits := msgBits shr 8;
     end;
-
-    (* process each block *)
+    
+    (* init registers *)
     a := InitA;
     b := InitB;
     c := InitC;
     d := InitD;
-    
-    
+
+    (* warning about unintialized buffer ? *)
+    for i := 0 to 15 do
+        buffer[i] := 0;
+
+    (* Process MD5 hash one message block at a time *)
+    for i := 0 to (blocks-1) do
+    begin
+        (* load buffer with padded message *)
+        tmp := i shl 6;
+        for j := 0 to 63 do
+        begin
+            buffIdx := j shr 2;
+
+            if tmp < msgBytes then
+                buffer[buffIdx] := (Ord(msg[tmp + 1]) shl 24) or (buffer[buffIdx] shr 8)
+            else
+                buffer[buffIdx] := (padded[tmp - msgBytes] shl 24) or (buffer[buffIdx] shr 8);
+            tmp := tmp + 1;
+        end;
+
+        (* init registers *)
+        a0 := a;
+        b0 := b;
+        c0 := c;
+        d0 := d;
+
+        (* apply transformations *)
+        for j := 0 to 63 do
+        begin
+            round := j shr 4;
+            buffIdx := j;
+            f := 0;
+
+            case round of
+                0:  (* Auxiliary function F *)
+                    begin
+                        f := (b and c) or ((not b) and d);
+                        buffIdx := j;
+                    end;
+                1:  (* Auxiliary function G *)
+                    begin
+                        f := (b and d) or (c and (not d));
+                        buffIdx := ((j * 5) + 1) and $0F;
+                    end;
+                2:  (* Auxiliary function H *)
+                    begin
+                        f := b xor c xor d;
+                        buffIdx := ((j * 3) + 5) and $0F;
+                    end;
+                3:  (* Auxiliary function I *)
+                    begin
+                        f := c xor (b or (not d));
+                        buffIdx := (j * 7) and $0F;
+                    end;
+            end;
+
+            tmp := b + RotateLeft32(a + f + buffer[buffIdx] + T[j], Shifts[(round * 4) or (j and 3)]);
+            a := d;
+            d := c;
+            c := b;
+            b := tmp;
+        end;
+
+        a := a + a0;
+        b := b + b0;
+        c := c + c0;
+        d := d + d0;
+    end;
+
+    (* finalize hash *)
+    buffIdx := 0;
+    tmp := 0;
+    for i := 0 to 3 do
+    begin
+        case i of 
+            0: tmp := a;
+            1: tmp := b;
+            2: tmp := c;
+            3: tmp := d;
+        end;
+        for j := 0 to 3 do
+        begin
+            hash[buffIdx] := tmp;
+            buffIdx := buffIdx + 1;
+            tmp := tmp >> 8;
+        end;
+    end;
+
+    (* return hash as string *)
+    hashStr := '0x';
+    for i := 0 to 15 do
+    begin
+        hashStr := hashStr + Lowercase(IntToHex(hash[i], 2));
+    end;
+
+    ComputeMD5 := hashStr;
 end;
 
+(* main *)
+var
+    i: integer;
+    actual: string;
 begin
+
+    Writeln('msg: "' + TestCases[2].Msg + '"');
+    Writeln('expected: ' + TestCases[2].Hash);
     ComputeMD5(TestCases[2].Msg);
+    Writeln('');
+
+    for i := 0 to (Length(TestCases) - 1) do
+    begin
+        Writeln('"' + TestCases[i].Msg + '"');
+        actual := ComputeMD5(TestCases[i].Msg);
+        assert(CompareStr(actual, TestCases[i].Hash) = 0, 'Calculated hash does not match test case');
+        Writeln('  actual:   ' + actual);
+        Writeln('  expected: ' + TestCases[i].Hash);
+        Writeln('');
+    end;
 end.
